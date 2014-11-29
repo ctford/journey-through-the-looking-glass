0a12f9771293cdccfe5621e9286fe6e6113f1813
Chris Ford <christophertford@gmail.com>
2014-11-29 19:30:35 +0000

    Initialise repository.

---
+++ src/journey_through_the_looking_glass/lens.clj
@@ -0,0 +1,6 @@
+(ns journey-through-the-looking-glass.lens)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/journey_through_the_looking_glass/test/lens.clj
@@ -0,0 +1,7 @@
+(ns journey-through-the-looking-glass.test.lens
+  (:require [clojure.test :refer :all]
+            [journey-through-the-looking-glass.lens :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
