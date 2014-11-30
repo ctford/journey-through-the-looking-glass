e5b060627b1b2383ac8fa3f600c2c45390e2d425
Chris Ford <christophertford@gmail.com>
2014-11-30 20:15:24 +0000

    Remove the protocols and rely on naked Functors.

--- src/journey_through_the_looking_glass/lens.clj
+++ src/journey_through_the_looking_glass/lens.clj
@@ -1,71 +1,33 @@
 (ns journey-through-the-looking-glass.lens)
 
-; Typeclasses
-(defprotocol Functor
-  (fmap [this f]))
-
+; Functors
 (defn fsequence [f] (partial map f))
-
-(defn fidentity [f] f)
-
-(defn fmaybe [f] #(some-> % f))
-
+(defn fidentity [f] (identity f))
+(defn fmaybe [f] (fn [x] (some-> x f)))
+(defn fpath [k f] (fn [x] (update-in x [k] f)))
 (defn fconstant [f] identity)
 
-(defn fpath [k f] #(update-in % [k] f))
+(defn inject [t functor]
+  (fn [f]
+    (fn [x]
+      ((functor (comp f t)) x))))
 
-; Instances
-(defrecord Identity [value]
-  Functor (fmap [this f] (-> value f ->Identity)))
-
-(defrecord Constant [value]
-  Functor (fmap [this _] this))
-
-(defrecord Maybe [value]
-  Functor (fmap [this f] (some-> value f ->Maybe)))
-
-(defrecord Sequence [value]
-  Functor (fmap [this f] (->> value (map f) ->Sequence)))
-
-; Functions
+; Lens operations
 (defn update [x lens f]
-  (-> x ((lens (comp ->Identity f))) :value))
+  ((lens (inject f fidentity)) x))
 
-(defn put [x lens y]
-  (update x lens (constantly y)))
+(defn put [x lens value]
+  (update x lens (constantly value)))
 
 (defn view [x lens]
-  (-> x ((lens ->Constant)) :value))
+  ((lens fconstant) x))
 
 ; Lenses
 (defn minutes [f]
-  (fn [x] (-> x (* 60) f (fmap (partial * 1/60)))))
+  (fn [x]
+    ((f #(/ % 60)) (* x 60))))
 
 (defn in [path]
   (fn [f]
     (fn [x]
-      (-> x (get-in path) f (fmap (partial assoc-in x path))))))
-
-; Traversals
-(defprotocol Applicative
-  (fapply [this f args]))
-
-(extend-protocol Applicative
-  Identity
-  (fapply [this f args] (->Identity (apply f (:value this) [(:value args)])))
-
-  Constant
-  (fapply [this _ args] (->Constant (apply concat (:value this) [(:value args)]))))
-
-(defn raise [x] (map (fn [y] [y]) x))
-(defn lower [x] (apply concat x))
-
-(defn updates [x lens f]
-  (-> x raise (update lens (partial map f)) lower))
-
-(defn each [f]
-  (fn [xs]
-    (reduce
-      (fn [accumulator x] (fapply (f x) cons accumulator))
-      []
-      (reverse xs))))
+      ((f (partial assoc-in x path)) (get-in x path)))))
--- test/journey_through_the_looking_glass/test/lens.clj
+++ test/journey_through_the_looking_glass/test/lens.clj
@@ -2,43 +2,29 @@
   (:require [midje.sweet :refer :all]
             [journey-through-the-looking-glass.lens :refer :all]))
 
-(fact "The identity Functor applies a function to a value."
-      (-> 3 ->Identity (fmap inc) :value) => 4
+(fact "The Identity Functor applies a function to a value."
       ((fidentity inc) 3) => 4)
 
-(fact "The constant Functor ignores any applied function."
-      (-> 3 ->Constant (fmap inc) :value) => 3
+(fact "The Constant Functor ignores any applied function."
       ((fconstant inc) 3) => 3)
 
-(fact "The maybe Functor only applies a function when a value is present."
-      (-> 3 ->Maybe (fmap inc) :value) => 4
-      (-> nil ->Maybe (fmap inc) :value) => nil
+(fact "The Maybe Functor only applies a function when a value is present."
       ((fmaybe inc) 3) => 4
       ((fmaybe inc) nil) => nil)
 
-(fact "The sequence Functor applies a function to each element in the sequence."
-      (-> [1 2 3] ->Sequence (fmap inc) :value) => [2 3 4]
+(fact "The Sequence Functor applies a function to each element in the sequence."
       ((fsequence inc) [1 2 3]) => [2 3 4])
+
+(fact "Functors compose."
+      (((comp fsequence fmaybe) inc) [1 nil 3]) => [2 nil 4])
 
 (fact "The path Functor applies a function to the value corresponding to a key within a map."
       ((fpath :x inc) {:x 1 :y 1}) => {:x 2 :y 1})
 
-(fact "The minutes lens supports the three lens operations."
-  (-> 3 (update minutes (partial + 30))) => 7/2
-  (-> 3 (view minutes)) => 180
-  (-> 3 (put minutes 120)) => 2)
+(fact "The minutes Lens supports the Lens operations."
+  (-> 3 (update minutes (partial + 60))) => 4
+  (-> 3 (view minutes)) => 180)
 
-(fact "The in lens supports the three lens operations."
+(fact "The in Lens supports the Lens operations."
   (-> {:x 1 :y 2} (update (in [:x]) inc)) => {:x 2 :y 2}
-  (-> {:x 1 :y 2} (view (in [:x]))) => 1
-  (-> {:x 1 :y 2} (put (in [:x]) 7)) => {:x 7 :y 2})
-
-(fact "Lenses compose with ordinary function composition."
-  (-> {:hours 1} (update (comp (in [:hours]) minutes) inc)) => {:hours 61/60}
-  (-> {:hours 1} (view (comp (in [:hours]) minutes))) => 60
-  (-> {:hours 1} (put (comp (in [:hours]) minutes) 120)) =>  {:hours 2})
-
-(fact "The each traversal iterates through a sequence."
-   (-> [[1] [2] [3]] (update each (partial map inc))) => [[2] [3] [4]]
-   (-> [1 2 3] (updates each inc)) => [2 3 4]
-   (-> [[1] [2] [3]] (view each)) => [1 2 3])
+  (-> {:x 1 :y 2} (view (in [:x]))) => 1)
