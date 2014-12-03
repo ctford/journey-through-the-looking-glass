05eef59e37592d1047167c706eb6274690d3cfab
Chris Ford <christophertford@gmail.com>
2014-12-03 21:02:28 +0000

    Update Lens examples.

--- src/journey_through_the_looking_glass/lens.clj
+++ src/journey_through_the_looking_glass/lens.clj
@@ -7,15 +7,24 @@
 (defn in [k f]
   (fn [m]
     (-> m
-        (get k)                          ; Deconstruct
-        f                                ; Apply function
-        (functor/fmap #(assoc m k %))))) ; Apply reconstruction
+        (get k)                            ; Deconstruct
+        f                                  ; Apply function
+        (functor/fmap #(assoc m k %)))))   ; Apply reconstruction
 
-(def up-to (comp functor/->Sequence range))
+(defn adjacent
+  [[x y]]
+  (functor/->Sequence
+    [[x (inc y)] [(inc x) y]
+     [(dec x) y] [x (dec y)]]))
 
 (fact "The In Lens focuses on a key's value."
-      ((in :x up-to) {:x 3 :y 2})
-      => (functor/->Sequence [{:x 0 :y 2} {:x 1 :y 2} {:x 2 :y 2}]))
+      (-> {:position [1 1] :class "Wizard"}
+          ((in :position adjacent)))
+      => (functor/->Sequence
+           [{:position [1 2], :class "Wizard"}
+            {:position [2 1], :class "Wizard"}
+            {:position [0 1], :class "Wizard"}
+            {:position [1 0], :class "Wizard"}]))
 
 
 (def apply-tax (partial * 1.10))
@@ -25,33 +34,32 @@
   (functor/->Maybe
     (try
       (Integer/parseInt string)
-      (catch Exception e
-        nil))))
+      (catch Exception e))))
 
 (fact "Using the Maybe Functor can return nil for the whole. "
       (-> {:price "90" :currency "AUD"}
           ((in :price number!?!?!))
-          (functor/fmap #(update-in % [:price] apply-tax)))
-      => (functor/->Maybe
-           {:price 99.00000000000001 :currency "AUD"})
+          (functor/fmap :price)
+          (functor/fmap apply-tax))
+      => (functor/->Maybe 99.00000000000001)
 
       (-> {:price "foobar" :currency "AUD"}
           ((in :price number!?!?!))
-          (functor/fmap #(update-in % [:price] apply-tax)))
-      => (functor/->Maybe
-           nil))
+          (functor/fmap :price)
+          (functor/fmap apply-tax))
+      => (functor/->Maybe nil))
 
 
 (defn minutes [f]
-  (fn [hours]
-    (-> hours
-        (* 60)                           ; Deconstruct
-        f                                ; Apply function
-        (functor/fmap #(/ % 60)))))      ; Apply reconstruction
+  (fn [seconds]
+    (-> seconds
+        (/ 60)                             ; Deconstruct
+        f                                  ; Apply function
+        (functor/fmap (partial * 60)))))   ; Apply reconstruction
 
 (fact "Lenses can focus on any view of a structure."
-      ((minutes (comp functor/->Identity (partial + 60))) 3)
-      => (functor/->Identity 4))
+      ((minutes (comp functor/->Identity inc)) 1)
+      => (functor/->Identity 61))
 
 
 ; Lens operations
@@ -70,11 +78,11 @@
 
 
 (fact "The In Lens supports the Lens operations."
-  (-> {:x 1 :y 2} (update (partial in :x) inc)) => {:x 2 :y 2}
-  (-> {:x 1 :y 2} (put (partial in :x) 99)) => {:x 99 :y 2}
-  (-> {:x 1 :y 2} (view (partial in :x))) => 1)
+      (-> {:x 1 :y 2} (update (partial in :x) inc)) => {:x 2 :y 2}
+      (-> {:x 1 :y 2} (put (partial in :x) 99)) => {:x 99 :y 2}
+      (-> {:x 1 :y 2} (view (partial in :x))) => 1)
 
 (fact "The Minutes Lens supports the Lens operations."
-  (-> 3 (update minutes (partial + 60))) => 4
-  (-> 3 (put minutes 60)) => 1
-  (-> 3 (view minutes)) => 180)
+      (-> 120 (update minutes dec)) => 60 
+      (-> 120 (put minutes 4)) => 240
+      (-> 120 (view minutes)) => 2)
