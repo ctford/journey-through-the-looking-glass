da72d7375bfe62394fb02c45a718ac8a2e341b25
Chris Ford <christophertford@gmail.com>
2015-07-02 20:07:10 +0100

    Add example of composition.

--- src/journey_through_the_looking_glass/maths.clj
+++ src/journey_through_the_looking_glass/maths.clj
@@ -10,3 +10,12 @@
 
 (fact "increment doesn't work on sequences."
       (increment [1 2 3]) => (throws ClassCastException))
+
+
+(def double-increment (comp increment increment))
+
+(fact "double-increment adds two to an integer."
+      (double-increment 1) => 3)
+
+(fact "double-increment doesn't work on sequences either."
+      (double-increment [1 2 3]) => (throws ClassCastException))
